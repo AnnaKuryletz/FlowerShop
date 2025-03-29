0d5b6363afc6de3197e7ae10e903d49788540ee9
anyukurilets <anyu.kutilez@mail.ru>
2025-03-30 01:16:27 +0300

    change settings

--- flowershop/flowershop/urls.py
+++ flowershop/flowershop/urls.py
@@ -20,8 +20,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Главная страница работает!")
 
 urlpatterns = [
+    path('', home, name='home'),
     path("admin/", admin.site.urls),
     path('api/', include('shop.urls')), 
 ]
