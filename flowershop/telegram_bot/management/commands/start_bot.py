e25223fe1a485a039bf1b90956c77504ca0c04b3
anyukurilets <anyu.kutilez@mail.ru>
2025-03-31 19:37:01 +0300

    change settings12

--- flowershop/telegram_bot/management/commands/start_bot.py
+++ flowershop/telegram_bot/management/commands/start_bot.py
@@ -1,5 +1,4 @@
 import asyncio
-import os
 import logging
 from django.core.management.base import BaseCommand
 from telegram_bot.bot import bot, dp  # Используем уже созданные объекты
@@ -8,11 +7,6 @@ logging.basicConfig(level=logging.INFO)
 
 async def main():
     logging.info("Бот запущен!")
-    db_path = "data/db.sqlite3"
-    if os.path.exists(db_path):
-        logging.info(f"✅ База данных найдена: {db_path}")
-    else:
-        logging.error("❌ База данных не найдена!")
     await dp.start_polling(bot)
 
 class Command(BaseCommand):
